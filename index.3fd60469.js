1aa3bbff73a4001fd18520fa9d078aaee555eb8e
Horizon-git <apbreloaded947@gmail.com>
2024-02-23 13:50:20 +0200

    make build

---
+++ index.3fd60469.js
@@ -0,0 +1,3 @@
+var e=document.querySelector(".tree");document.querySelectorAll("li").forEach(function(e){if(e.querySelector("ul")){var t=document.createElement("span");e.insertBefore(t,e.firstChild),t.appendChild(t.nextSibling)}}),e.addEventListener("click",function(e){var t=e.target;if("SPAN"===t.tagName){var r=t.parentNode.querySelector("ul");r.style.display="none"===r.style.display?"block":"none"}});//# sourceMappingURL=index.3fd60469.js.map
+
+//# sourceMappingURL=index.3fd60469.js.map
